596c4d6cdca799b1b167d16c5e1644b0efd9eee4
Luke Russell <thelukerussel21@gmail.com>
2020-01-30 17:21:56 -0500

    added a function that randomizes each attack of a character

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const btn = document.getElementById("myBtn");
 
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// const span = document.getElementsByClassName("close")[0];
 
 
 // When the user clicks on the button, open the modal
@@ -20,16 +20,18 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
 
 class Hero {
   constructor(options) {
@@ -37,38 +39,59 @@ class Hero {
 
     this.name = options.name;
     this.attack = options.attack;
+
+    this.heroDamage = function(){
+      let hit = Math.floor(Math.random() * 15) + 5;
+      // console.log(hit);
   }
 }
+}
 
-class Enemy {
+class Enemy{
   constructor(options) {
     options = options || {};
 
     this.name = options.name;
     this.attack = options.attack;
+
+    this.enemyDamage = function(){
+      let hit = Math.floor(Math.random() * 15) + 5;
+      console.log(hit);
   }
+}
 }
 
 let scorpian = new Hero({
   name: 'Scorpian',
   attack: 'sting',
+  health: 100,
+
   image: '/images/yellow-guy-resize.png'
 });
 
 let subZero = new Hero({
   name: 'Sub Zero',
   attack: 'frostbite',
+  health: 100,
+
   image: '/images/sub-zero.png'
 });
 
 let freddy = new Enemy({
   name: 'Freddy',
   attack: 'nightmare',
+  health: 100,
+
   image: '/images/freddy.png'
 })
 
 let ermac = new Enemy({
   name: 'Ermac',
   attack: 'crush',
+  health: 100,
+
   image: '/images/green-guy.png'
 })
+
+let heros = [scorpian, subZero];
+let enemies = [freddy, ermac];
